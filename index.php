5dc47b31143a4c477fe4c939b040d9ec252d8ae7
Michael Lyon <lyoncodes@gmail.com>
2022-03-07 16:05:31 -0800

    adds feed caching starting logic

--- index.php
+++ index.php
@@ -8,26 +8,55 @@ if (!isset($_SESSION['username'])) {
 } else {
   // else we're good to go
   echo '<p>hello '.$_SESSION['username'].', your user id is: '. $_SESSION['id'].'</p>';
+  
   // query the cache
-  // if no cache hit...
+  if (isset($_SESSION['cache']) && count($_SESSION['cache']) > 0) {
+    echo 'this is from the cache';
+    echo '<br>';
+    foreach($_SESSION['cache'] as $cached){
+      // var_dump($_SESSION['cache']);
+      // echo '<br>';
+      echo '<pre>'.var_dump($cached).'</pre>';
+      // print '<h1>' . $cached->channel->title . '</h1>';
+      // foreach($cached->channel->item as $story)
+      // {
+      //   echo '<a href="' . $story->link . '">' . $story->title . '</a><br />'; 
+      //   echo '<p>' . $story->description . '</p><br /><br />';
+      // }
+    }
+  } else {
+    // if no cache hit...
+    
+    // make a round trip to the server
+    $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+    $query = "SELECT f.URL, f.title FROM category c JOIN feed f ON c.categoryID = f.categoryID WHERE c.userID = ";
+    $query .= $_SESSION['id'];
+
+    $res = mysqli_query($conn, $query);
+    
+    while ($row = mysqli_fetch_assoc($res)){
+      $response = file_get_contents($row['URL']);
+      $xml = simplexml_load_string($response);
+      foreach($xml->channel->item as $feedItem) {
+        // echo $feedItem->link;
+        echo '<br>';
+        echo '<h1>'.$feedItem->title.'</h1>';
+        echo '<br>';
+        echo $feedItem->description;
+        echo '<br>';
+        $_SESSION['cache'][] = $feedItem->asXML();
+      }
+    }
+
+    // foreach($_SESSION['cache'] as $cached){
+    //   print '<h1>' . $cached->channel->title . '</h1>';
+    //   foreach($cached->channel->item as $story)
+    //   {
+    //     echo '<a href="' . $story->link . '">' . $story->title . '</a><br />'; 
+    //     echo '<p>' . $story->description . '</p><br /><br />';
+    //   }
+    // }
   
-  // make a round trip to the server
-  $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-  $sql = "SELECT (feed.URL) FROM category JOIN feed ON category.categoryID = feed.categoryID WHERE userID = ";
-  $sql .= $_SESSION['id'];
-  $query = mysqli_query($conn, $sql);
-  $results = mysqli_fetch_assoc($query);
-  $url =  ($results["URL"]);
-  
-  
-  // visit the fetched url and call the xml
-  $response = file_get_contents($url);
-  $xml = simplexml_load_string($response);
-  print '<h1>' . $xml->channel->title . '</h1>';
-  foreach($xml->channel->item as $story)
-  {
-    echo '<a href="' . $story->link . '">' . $story->title . '</a><br />'; 
-    echo '<p>' . $story->description . '</p><br /><br />';
   }
 }
 
