bc29ee37787cabe95532710c66bf216ebeb49f06
Michael Lyon <lyoncodes@gmail.com>
2022-03-10 08:39:01 -0800

    adds new db ddl

--- dbModel.sql
+++ dbModel.sql
@@ -15,8 +15,7 @@ CREATE TABLE category(
 CREATE TABLE feed(
   feedID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   categoryID INT REFERENCES category(categoryID),
-  title VARCHAR(255),
-  URL VARCHAR(255)
+  name VARCHAR(255)
 );
 
 INSERT INTO users VALUES(1, 'ex_user', 'p@ssword');
@@ -24,15 +23,15 @@ INSERT INTO category VALUES(1, 1, 'finance');
 INSERT INTO category VALUES(2, 1, 'technology');
 INSERT INTO category VALUES(3, 1, 'sports');
 
-INSERT INTO feed VALUES(1, 1, 'cryptocurrency', 'https://news.google.com/rss/search?q=crypto+currency&hl=en-US&gl=US&ceid=US:en');
-INSERT INTO feed VALUES(2, 1, 'exchange rates', 'https://news.google.com/rss/search?q=exchange+rates&hl=en-US&gl=US&ceid=US:en');
-INSERT INTO feed VALUES(3, 1, 'stock market', 'https://news.google.com/rss/search?q=stock+market&hl=en-US&gl=US&ceid=US:en');
+INSERT INTO feed VALUES(1, 1, 'cryptocurrency');
+INSERT INTO feed VALUES(2, 1, 'exchange rates');
+INSERT INTO feed VALUES(3, 1, 'stock market');
 
-INSERT INTO feed VALUES(4, 2, 'artificial intelligence', 'https://news.google.com/rss/search?q=artificial+intelligence&hl=en-US&gl=US&ceid=US:en');
-INSERT INTO feed VALUES(5, 2, 'cybersecurity', 'https://news.google.com/rss/search?q=cyber+security&hl=en-US&gl=US&ceid=US:en');
-INSERT INTO feed VALUES(6, 2, 'robotics', 'https://news.google.com/rss/search?q=robotics&hl=en-US&gl=US&ceid=US:en');
+INSERT INTO feed VALUES(4, 2, 'artificial intelligence');
+INSERT INTO feed VALUES(5, 2, 'cybersecurity');
+INSERT INTO feed VALUES(6, 2, 'robotics');
 
-INSERT INTO feed VALUES(7, 3, 'nfl', 'https://news.google.com/rss/search?q=national+football+league&hl=en-US&gl=US&ceid=US:en');
-INSERT INTO feed VALUES(8, 3, 'nba', 'https://news.google.com/rss/search?q=national+basketball+association&hl=en-US&gl=US&ceid=US:en');
-INSERT INTO feed VALUES(9, 3, 'mlb', 'https://news.google.com/rss/search?q=major+league+baseball&hl=en-US&gl=US&ceid=US:en');
+INSERT INTO feed VALUES(7, 3, 'nfl');
+INSERT INTO feed VALUES(8, 3, 'nba');
+INSERT INTO feed VALUES(9, 3, 'mlb');
 
